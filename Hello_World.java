938e4a65be620dc649c30a92c5e609d7bcaf864c
Datboi-Dan <datboidan961@gmail.com>
2024-08-14 10:32:05 -1000

    Update Hello_World.java
    haro warudo

--- Hello_World.java
+++ Hello_World.java
@@ -1,1 +1,17 @@
+/*
+ * This code is for the viery first assignment. It does two simple prints and I submit it.
+ */
+public class Hello_World {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Task 1: Print out "Hello World"
+		System.out.print("Hello World");
+		
+		//Task 2: Print out "Hello World" and then print it again on the next line
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+	}
+
+}
