6a8ef64c2e3ae6eccfb384ca9e647d777332f37a
Datboi-Dan <datboidan961@gmail.com>
2024-08-26 14:15:22 -1000

    Update Modulus_CompoundOperators_And__Casting_Practice_1.java

--- Modulus_CompoundOperators_And__Casting_Practice_1.java
+++ Modulus_CompoundOperators_And__Casting_Practice_1.java
@@ -17,3 +17,17 @@
 
      System.out.println(881 % 2);
 //prints 1
+
+
+//PART THREE: CASTING
+
+    int total = 5 + 7 + 2;
+    (double) (total / 3); //Option A
+    total / 3; //Option B
+    (double) total / 3; //Option C
+
+//Which of the above options correctly computes the average of total? Why do the other options not work?
+/*
+* The correct option is C, because the when the division occurs in the first two, both numbers are ints, so the quotient would be truncated.
+* In option C though, the total is cast as a double. Since doubles take priority over ints, the answer would remain in decimal form, which we want.
+*/
